755659fed3f8275a1414bb9031a2fed3849a1080
Worty <lethanhdat762003@gmail.com>
2025-03-21 08:42:07 +0700

    feat(auth): add login

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,9 +1,13 @@
 "use client";
 
+import { LoginForm } from "@/components/auth/login-form";
+
 export default function LoginPage() {
   return (
-    <>
-      <div>Hello World</div>
-    </>
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <LoginForm />
+      </div>
+    </div>
   );
 }
--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -13,7 +13,6 @@ import {
   ShoppingCart,
   Users,
 } from "lucide-react";
-
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import {
